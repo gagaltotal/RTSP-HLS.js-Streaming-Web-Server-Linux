cc80d5de6a36d0d71d4ce6ba09be77984f4c0766
Tot666 <gattimusprime@gmail.com>
2022-05-23 19:49:43 +0700

    Update cctv-rtsp.sh

--- cctv-rtsp.sh
+++ cctv-rtsp.sh
@@ -3,4 +3,4 @@ VIDSOURCE="rtsp://192.168.100.254:7447/5f97b841848ef75490fd9824_1"
 AUDIO_OPTS="-c:a aac -b:a 160000 -ac 2"
 VIDEO_OPTS="-s 854x480 -c:v libx264 -b:v 2048000"
 OUTPUT_HLS="-hls_time 6 -hls_list_size 6 -hls_wrap 6"
-ffmpeg -rtsp_transport tcp -v verbose -i "$VIDSOURCE" -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS /var/www/html/live/tamansari.m3u8
+ffmpeg -rtsp_transport tcp -v verbose -i "$VIDSOURCE" -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS /var/www/html/live/namacctv.m3u8
