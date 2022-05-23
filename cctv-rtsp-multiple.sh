6a8cc11c993c6d003563e6b5372374659acc78bd
Tot666 <gattimusprime@gmail.com>
2022-05-23 19:58:37 +0700

    Update cctv-rtsp-multiple.sh

--- cctv-rtsp-multiple.sh
+++ cctv-rtsp-multiple.sh
@@ -4,7 +4,7 @@
 
 #link url RTSP
 cctv1="rtsp://ayang:liatayang@192.168.111.104:554/Streaming/channels/101"
-cctv2="rtsp://ayang:liatayang192.168.111.109:554/Streaming/channels/101"
+cctv2="rtsp://ayang:liatayang@192.168.111.109:554/Streaming/channels/101"
 cctv3="rtsp://192.168.100.254:7447/5e464590848e2cc5ec3e5fef_1"
 cctv4="rtsp://192.168.100.254:7447/5f97b841848ef75490fd9824_1"
 cctv5="rtsp://192.168.100.254:7447/5fe04797848e7febe162eb1f_1"
